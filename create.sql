b1d6068e7e532e28636ec5621a4ba53f764dd69f
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2021-11-14 00:40:22 -0300

    finalizado

--- create.sql
+++ create.sql
@@ -1,22 +1,5 @@
-
-    create table tb_cliente (
-       id integer not null auto_increment,
-        dn varchar(255),
-        nome varchar(255),
-        primary key (id)
-    ) engine=InnoDB;
-
-    create table tb_compras (
-       id integer not null auto_increment,
-        data_compra date,
-        total_compra double precision,
-        cliente_id integer,
-        primary key (id)
-    ) engine=InnoDB;
-
-    alter table tb_compras 
-       add constraint FKto1hl9btn8n1ssr1yglhog601 
-       foreign key (cliente_id) 
-       references tb_cliente (id);
+create table tb_cliente (id  serial not null, dn varchar(255), nome varchar(255), primary key (id));
+create table tb_compras (id  serial not null, data_compra date, total_compra float8, cliente_id int4, primary key (id));
+alter table tb_compras add constraint FKto1hl9btn8n1ssr1yglhog601 foreign key (cliente_id) references tb_cliente;
 INSERT INTO tb_cliente(nome,dn) VALUES ('Maria','14/12/2000');
 INSERT INTO tb_compras(cliente_id,total_compra,data_compra) VALUES (1, 100.0,'2021-05-15');
